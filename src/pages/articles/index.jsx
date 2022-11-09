72aeabfc4ff66485037d0f949d72702b3ee8aaae
charliesabino <94320712+charliesabino@users.noreply.github.com>
2022-11-09 14:28:55 -0600

    updated articles page

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -41,12 +41,12 @@ export default function ArticlesIndex({ articles }) {
         <title>Articles - Charlie Sabino</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="All of my long-form thoughts on programming, technology, and any other interesing topics"
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Writing on whatever I find interesting."
+        intro="All of my long-form thoughts on programming, technology, and any other interesing topics"
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
